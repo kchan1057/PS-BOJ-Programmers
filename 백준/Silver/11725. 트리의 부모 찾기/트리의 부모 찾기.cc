380f6a919b0a309780fa811f3f305bbc4e5fde90
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-04 16:18:54 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 40 ms, Memory: 8060 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -1,32 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> adj[100001];
-int ans[100001];
-bool vis[100001];
-int n;
-int main(void)
-{
-    ios::sync_with_stdio(0); cin.tie(0);
-    cin >> n;
-    for (int i = 0; i < n - 1; i++) {
+vector<int> adj[100005];
+int p[100005];
+void bfs(int root){
+    queue<int> q;
+    q.push(root);
+    while(!q.empty()){
+        int cur = q.front(); q.pop();
+        for(int nxt : adj[cur]){
+            if(p[cur] == nxt) continue;
+            q.push(nxt);
+            p[nxt] = cur;
+        }
+    }
+}
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int n; cin >> n;
+    for(int i = 0; i < n-1; i++){
         int a, b; cin >> a >> b;
         adj[a].push_back(b);
         adj[b].push_back(a);
     }
-    queue<int> q;
-    q.push(1); vis[1] = true; // 1번 노드에서 시작
-
-    while (!q.empty()) {
-        int cur = q.front(); q.pop();
-        for (auto c : adj[cur]) {
-            if (vis[c]) continue; // 이미 방문한 노드는 건너뜀
-            ans[c] = cur; // 현재 노드가 자식 노드의 부모
-            vis[c] = true; 
-            q.push(c);
-        }
-    }
-    for (int i = 2; i <= n; i++) // 2번 노드부터 부모 출력
-        cout << ans[i] << '\n';
-
+    bfs(1);
+    for(int i = 2; i <= n; i++) cout << p[i] << '\n';
     return 0;
 }
