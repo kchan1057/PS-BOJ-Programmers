fee1038d15f56fc39f959ed670ba9f5a088ba098
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-13 21:24:45 +0900

    [Silver IV] Title: 상근이의 여행, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T; cin >> T;
+    while(T--) {
+        int n, m; cin >> n >> m;
+        for(int i = 0; i < m; i++){
+            int a, b; cin >> a >> b;
+        }
+        cout << n-1 << '\n';
+    }
+    return 0;
+}
