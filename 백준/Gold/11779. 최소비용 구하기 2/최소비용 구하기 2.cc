e2244351d38d291f4a6204aadbfa6a80c4cd420b
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-18 13:45:27 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 32 ms, Memory: 4752 KB -BaekjoonHub

---
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.cc
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define X first
+#define Y second
+using namespace std;
+vector<pair<int, int>> adj[1005];
+int dist[1005];
+int pre[1005];
+const int INF = 0x3f3f3f3f;
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int n, m; cin >> n >> m;
+    for(int i = 0; i < m; i++){
+        int a, b, cost; cin >> a >> b >> cost;
+        adj[a].push_back({cost, b});
+    }
+    int st, en; cin >> st >> en;
+    fill(dist, dist+1+n, INF);
+    dist[st] = 0, pre[st] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, st});
+    while(!pq.empty()){
+        auto cur = pq.top(); pq.pop();
+        if(dist[cur.Y] < cur.X) continue;
+        for(auto nxt : adj[cur.Y]){
+            if(dist[cur.Y] + nxt.X < dist[nxt.Y]){
+                dist[nxt.Y] = dist[cur.Y] + nxt.X;
+                pq.push({dist[nxt.Y], nxt.Y});
+                pre[nxt.Y] = cur.Y;
+            }
+        }
+    }
+    vector<int> result;
+    int cur = en;
+    while(cur != 0){
+        result.push_back(cur);
+        cur = pre[cur];
+    }
+    cout << dist[en]-dist[st] << '\n';
+    cout << result.size() << '\n';
+    reverse(result.begin(), result.end());
+    for(auto nxt : result) cout << nxt << ' ';
+    return 0;
+}
