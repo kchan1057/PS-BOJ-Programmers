5d92bdb13dca29482036a1082f486b5024741568
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-25 15:56:28 +0900

    [Gold IV] Title: 빙산, Time: 64 ms, Memory: 2976 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -1,58 +1,76 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define X first
 #define Y second
-using namespace std;
+int n, m;
 int board[303][303];
-bool vis[303][303];
+int vis[303][303];
 int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
+void bfs(int i, int j) {
+    queue<pair<int, int>> q;
+    q.push({i, j});
+    vis[i][j] = 1;
+    while (!q.empty()) {
+        auto cur = q.front(); q.pop();
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = cur.X + dx[dir];
+            int ny = cur.Y + dy[dir];
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if (board[nx][ny] > 0 && !vis[nx][ny]) {
+                vis[nx][ny] = 1;
+                q.push({nx, ny});
+            }
+        }
+    }
+}
+void melt() {
+    int temp[303][303] = {0,};
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (board[i][j] == 0) continue;
+            int sea_cnt = 0;
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = i + dx[dir];
+                int ny = j + dy[dir];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if (board[nx][ny] == 0) sea_cnt++;
+            }
+            temp[i][j] = max(0, board[i][j] - sea_cnt);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) board[i][j] = temp[i][j];
+    }
+}
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
-    int n, m; cin >> n >> m;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++) cin >> board[i][j];
+    cin.tie(0); cout.tie(0);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) cin >> board[i][j];
     }
-    int area = 0, cnt = 0;
-    queue<pair<int, int>> Q;
-    while(1){
-        area = 0;
-        memset(vis, false, sizeof(vis));
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(board[i][j] != 0 && vis[i][j] == 0){
-                    Q.push({i, j});
-                    vis[i][j] = 1;
-                    area++;
-                }
-                while(!Q.empty()){
-                    auto cur = Q.front(); Q.pop();
-                    for(int dir = 0; dir < 4; dir++){
-                        int nx = dx[dir] + cur.X;
-                        int ny = dy[dir] + cur.Y;
-                        if(nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-                        if(vis[nx][ny]) continue;
-                        if(board[nx][ny] == 0){
-                            if(board[cur.X][cur.Y] > 0) board[cur.X][cur.Y]--;
-                        }
-                        else{
-                            Q.push({nx, ny});
-                            vis[nx][ny] = 1;
-                        }
-                    }
-
+    int year = 0;
+    while (true) {
+        int chunks = 0;
+        memset(vis, 0, sizeof(vis));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] > 0 && !vis[i][j]) {
+                    chunks++;
+                    bfs(i, j);
                 }
             }
         }
-        cnt++;
-        if(area > 1){
-            cout << cnt-1;
+        if (chunks >= 2) {
+            cout << year;
             return 0;
         }
-        if(area == 0){
+        if (chunks == 0) {
             cout << 0;
             return 0;
         }
+        melt(); 
+        year++;
     }
-    return 0;
 }
