25148bd3ceb9cbead521474ea8d7f2f4c43bff70
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-26 17:01:29 +0900

    [Bronze V] Title: Plane, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int a, b, c, d;
+int main(){
+	cin.tie(0); cout.tie(0);
+	ios::sync_with_stdio(0);
+	cin >> a >> b >> c >> d;
+	cout << a * b + c * d << endl;
+}
