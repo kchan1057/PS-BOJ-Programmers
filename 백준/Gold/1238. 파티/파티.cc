d51a3521a3b74656f47956f7b31016e511be4242
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-02-26 16:42:58 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2344 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -1,48 +1,50 @@
 #include <bits/stdc++.h>
+using namespace std;
 #define X first
 #define Y second
-using namespace std;
 vector<pair<int, int>> adj[1005];
-int dist1[10005], dist2[10005];
-vector<int> result;
+vector<pair<int, int>> adj_rev[1005];
 const int INF = 0x3f3f3f3f;
+int dist[1005];
+int dist_rev[1005];
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0); cout.tie(0);
     int n, m, x; cin >> n >> m >> x;
-    vector<int> result;
     for(int i = 0; i < m; i++){
         int a, b, cost; cin >> a >> b >> cost;
         adj[a].push_back({cost, b});
+        adj_rev[b].push_back({cost, a});
     }
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    fill(dist1, dist1+10005, INF);
-    pq.push({0, x}); dist1[x] = 0;
+    fill(dist, dist+1005, INF); 
+    pq.push({0, x}); dist[x] = 0;
     while(!pq.empty()){
         auto cur = pq.top(); pq.pop();
-        if(dist1[cur.Y] < cur.X) continue;
+        if(dist[cur.Y] < cur.X) continue;
         for(auto nxt : adj[cur.Y]){
-            if(dist1[cur.Y] + nxt.X < dist1[nxt.Y]){
-                dist1[nxt.Y] = dist1[cur.Y] + nxt.X;
-                pq.push({dist1[nxt.Y], nxt.Y});
+            if(dist[cur.Y] + nxt.X < dist[nxt.Y]){
+                dist[nxt.Y] = dist[cur.Y] + nxt.X;
+                pq.push({dist[nxt.Y], nxt.Y});
             }
         }
     }
+    fill(dist_rev, dist_rev + 1005, INF);
+    dist_rev[x] = 0; pq.push({0, x});
+    while(!pq.empty()){
+        auto cur = pq.top(); pq.pop();
+        if(dist_rev[cur.Y] < cur.X) continue;
+        for(auto nxt : adj_rev[cur.Y]){
+            if(dist_rev[cur.Y] + nxt.X < dist_rev[nxt.Y]){
+                dist_rev[nxt.Y] = dist_rev[cur.Y] + nxt.X;
+                pq.push({dist_rev[nxt.Y], nxt.Y});
+            }
+        }
+    }
+    int max = 0;
     for(int i = 1; i <= n; i++){
-        fill(dist2, dist2+10005, INF);
-        dist2[i] = 0; pq.push({0, i});
-        while(!pq.empty()){
-            auto cur = pq.top(); pq.pop();
-            if(dist2[cur.Y] < cur.X) continue;
-            for(auto nxt : adj[cur.Y]){
-                if(dist2[cur.Y] + nxt.X < dist2[nxt.Y]){
-                    dist2[nxt.Y] = dist2[cur.Y] + nxt.X;
-                    pq.push({dist2[nxt.Y], nxt.Y});
-                }
-            }
-        }
-        result.push_back(dist2[x] + dist1[i]);
+        if(max < dist[i] + dist_rev[i] && dist[i]+dist_rev[i] < INF) max = dist[i] + dist_rev[i];
     }
-    cout << *max_element(result.begin(), result.end());
-    return 0;
+    cout << max;
+    return 0;   
 }
