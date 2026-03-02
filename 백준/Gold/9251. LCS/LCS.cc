9d5143d62bf1ed4f12838f10397d2acad3b70113
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-03-02 18:01:57 +0900

    [Gold IV] Title: LCS, Time: 4 ms, Memory: 5968 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int dp[1005][1005];
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    string a, b; cin >> a >> b;
+    for(int i = 1; i <= a.length(); i++){
+        for(int j = 1; j <= b.length(); j++){
+            if(a[i-1] == b[j-1]) dp[i][j] = max(dp[i-1][j-1] + 1, dp[i][j]);
+            dp[i][j] = max({dp[i][j], dp[i-1][j], dp[i][j-1]});
+        }
+    }
+    cout << dp[a.length()][b.length()];
+    return 0;   
+}
