526d87f76ffde09d2cd7477962cb5fe8ca742502
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-03-18 16:41:34 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 112 ms, Memory: 15652 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,4 +1,6 @@
 #include <bits/stdc++.h>
+#define X first
+#define Y second
 using namespace std;
 int board[1005][1005];
 int dist[1005][1005][2];
@@ -6,42 +8,41 @@ int dx[4] = {1, 0, -1, 0};
 int dy[4] = {0, 1, 0, -1};
 int main() {
     ios::sync_with_stdio(0);
-    cin.tie(0);
+    cin.tie(0); cout.tie(0);
     int n, m; cin >> n >> m;
-    for(int i = 1; i <= n; i++){
-        string st; cin >> st;
-        for(int j = 1; j <= m; j++) board[i][j] = (st[j-1] - '0');
+    for(int i = 0; i < n; i++){
+        string a; cin >> a;
+        for(int j = 0; j < m; j++) board[i+1][j+1] = a[j] -'0';
     }
-    memset(dist, -1, sizeof(dist));
-    if(n == 1 && m == 1) {
+    if(n == 1 && m == 1){
         cout << 1;
         return 0;
     }
+    memset(dist, -1, sizeof(dist));
     queue<tuple<int, int, int>> Q;
-    Q.push({1, 1, 0});
-    dist[1][1][0] = 1;
-    while(!Q.empty()){
-        int x, y, broken;
-        tie(x, y, broken) = Q.front(); Q.pop();
+    Q.push({1, 1, 0}); dist[1][1][0] = 1;
+    while(!Q.empty()) {
+        int a, b, state;
+        tie(a, b, state) = Q.front(); Q.pop();
         for(int dir = 0; dir < 4; dir++){
-            int nx = x + dx[dir];
-            int ny = y + dy[dir];
+            int nx = dx[dir] + a;
+            int ny = dy[dir] + b;
             if(nx < 1 || ny < 1 || nx > n || ny > m) continue;
-            if(board[nx][ny] == 1 && broken == 0) {
-                if(dist[nx][ny][1] == -1) {
-                    dist[nx][ny][1] = dist[x][y][0] + 1;
+            if(dist[nx][ny][state] != -1) continue;
+            if(nx == n && ny == m){
+                cout << dist[a][b][state] + 1;
+                return 0;
+            }
+            if(board[nx][ny] == 1) {
+                if(state == 0){
+                    dist[nx][ny][1] = dist[a][b][0] + 1;
                     Q.push({nx, ny, 1});
                 }
+                else continue;
             }
-            else if(board[nx][ny] == 0) {
-                if(dist[nx][ny][broken] == -1) {
-                    dist[nx][ny][broken] = dist[x][y][broken] + 1;
-                    Q.push({nx, ny, broken});
-                }
-            }
-            if(nx == n && ny == m) {
-                cout << dist[nx][ny][broken];
-                return 0;
+            else{
+                dist[nx][ny][state] = dist[a][b][state] + 1;
+                Q.push({nx, ny, state});
             }
         }
     }
