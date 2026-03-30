ae084865c4f031d67fd32e8541d8954419547ea5
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-03-31 01:26:14 +0900

    [Gold III] Title: 텀 프로젝트, Time: 480 ms, Memory: 8784 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.cc
@@ -1,32 +1,39 @@
 #include <bits/stdc++.h>
+#define X first
+#define Y second
 using namespace std;
-int main() {
+
+int n;
+int dig[100005];
+vector<int> result;
+vector<int> adj[100005];
+int main(){
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
+
     int T; cin >> T;
-    while(T--){
-        int n; cin >> n;
-        vector<int> adj[100005] = {};
-        int dig[100005] = {};
-        vector<int> result = {};
+    while(T--) {
+        for(int i = 0; i <= n; i++) adj[i].clear();
+        result.clear();
+        memset(dig, 0, sizeof(dig));
+        cin >> n;
         for(int i = 1; i <= n; i++){
-            int k; cin >> k;
-            adj[i].push_back(k);
-            dig[k]++;
+            int a; cin >> a;
+            adj[i].push_back(a);
+            dig[a]++;
         }
         queue<int> Q;
         for(int i = 1; i <= n; i++){
-            if(dig[i] == 0){
+            if(dig[i] == 0) {
                 Q.push(i);
                 result.push_back(i);
-            } 
+            }
         }
-
         while(!Q.empty()){
             int cur = Q.front(); Q.pop();
             for(int nxt : adj[cur]){
                 dig[nxt]--;
-                if(dig[nxt] == 0){
+                if(dig[nxt] == 0) {
                     Q.push(nxt);
                     result.push_back(nxt);
                 }
@@ -34,5 +41,5 @@ int main() {
         }
         cout << result.size() << '\n';
     }
-    return 0;   
+    return 0;
 }
