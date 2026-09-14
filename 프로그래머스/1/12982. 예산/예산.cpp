05591b20b3cb9c4b31fa44e97d7ac60b6883bd09
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-09-15 01:02:39 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.86 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    sort(d.begin(), d.end());
+    int ans = 0;
+    
+    for(int i = 0; i < d.size(); i++){
+        if(budget - d[i] >= 0){
+            ans++;
+            budget -= d[i];
+        }
+        else return ans;
+    }
+    return ans;
+}
