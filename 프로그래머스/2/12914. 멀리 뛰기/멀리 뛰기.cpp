06ccb435e6024f5aa0bc8d9bfaa7cd2e71877be3
sg.chan <163969084+kchan1057@users.noreply.github.com>
2026-09-24 01:08:53 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.01 ms, Memory: 4.61 MB -BaekjoonHub

---
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+long long dp[2005];
+long long solution(int n) {
+    dp[0] = 0, dp[1] = 1, dp[2] = 2;
+    for(int i = 3; i <= n; i++) dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    return dp[n];
+}
